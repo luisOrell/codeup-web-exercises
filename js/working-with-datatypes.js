6be3851f6eda20a5df22c1fa45ffaedc5f59f10f
Luis Orellana <luis.m.orellana01@gmail.com>
2023-10-12 15:11:39 -0500

    working with datatypes and variables exercise

---
+++ js/working-with-datatypes.js
@@ -0,0 +1,17 @@
+let sample = 'Hello Codeup';
+console.log(sample.length);
+
+// Uppercase
+console.log(sample.toUpperCase());
+
+// Lowercase
+console.log(sample.toLowerCase());
+
+//concanate sample to add students
+sample =+ " Students"
+console.log(sample);
+// +=  shorthand method for sample = sample + "students"
+
+// replace students with class
+console.log(sample.replace("Students", "Class"))
+
